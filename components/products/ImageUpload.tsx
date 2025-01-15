1cac265272adc5880384a7f892b61b0a466af21a
Juan-Valenzuela3 <juan_svalenzuela@soy.sena.edu.co>
2025-01-15 16:54:14 -0500

    Fix image

--- components/products/ImageUpload.tsx
+++ components/products/ImageUpload.tsx
@@ -17,7 +17,7 @@ export default function ImageUpload({image} : {image: string | undefined}) {
                     setImageUrl(result.info?.secure_url)
                 }
             }}
-            uploadPreset='utgx56zx'
+            uploadPreset='preset'
             options={{
                 maxFiles: 1
             }}
